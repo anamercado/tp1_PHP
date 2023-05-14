2a11cbdb60bcc17fef8f730c2617b7646dbd9eca
Santicabral <cabralsantiagogabriel@gmail.com>
2023-05-13 23:08:07 -0300

    formulario contacto en index y archivo contacto php

---
+++ datos.php
@@ -0,0 +1,28 @@
+<?php 
+
+$nombre     = '';
+$apellido   = '';
+$edad       = '';
+$email       = '';
+
+// Metodo isset()
+// Nos permite validar que el campo que esperamos, llegue.
+if (isset($_POST["nombre"]) && $_POST["nombre"]!='') { // si isset devuelve true, significa que el campo vino
+    $nombre   = $_POST["nombre"];
+} else {
+    // Redireccionamos a una pagina de error, o al formulario con un mensaje que informe que no vino el campo
+    header("location:index.php?error=Falta el nombre");
+}
+
+if (isset($_POST["apellido"])) { // si isset devuelve true, significa que el campo vino
+    $apellido   = $_POST["apellido"];
+}
+if (isset($_POST["edad"])) { // si isset devuelve true, significa que el campo vino
+    $edad   = $_POST["edad"];
+}
+if (isset($_POST["email"])) { // si isset devuelve true, significa que el campo vino
+    $email   = $_POST["email"];
+}
+
+echo $nombre." ".$apellido." tiene ".$edad." años";
+?>
